3e3ff6097524edbd16b58d4c4c8683ef1f88de33
peusants <pedrokinagase@gmail.com>
2021-10-24 22:18:50 -0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,19 @@
+. Construi um formulário mais simples possível, usei as cores que a empresa Itaú representa, Azul e Laranja.
+. Coloquei o icone e o titulo na aba, depois criei uma faixa azul no topo para mostrar o que seria o formulário.
+. Depois, botei todos os campos o que foi pedido, ajeitei o campo, tirando os lados da caixa do input e mudando a cor da linha.
+. Também tem os campos obrigatórios, principalmente no CPF, ele avisa automaticamente se está válido ou não.
+. No botão "Cadastrar" coloquei o "hover" para que ele mudasse de cor quando passar o mouse lá.
+. Ao apertar no "Cadastrar", aparece outra 'página' mostrando que o formulário está funcionando.
+
+Links da Pesquisa: 
+Validando CPF: https://www.devmedia.com.br/validar-cpf-com-javascript/23916
+Colocando o ícone no título: https://www.ti-enxame.com/pt/html/adicionar-imagem-na-barra-de-titulo/1072473781/
+Icone da Itau: https://logodownload.org/itau-logo/
+Imagem do Fundo: https://br.freepik.com/vetores-gratis/fundo-abstrato-de-meio-tom_9009351.htm
+
+
+Informações:
+
+Site:
+Github:
+LinkedIn: https://www.linkedin.com/in/pedro-de-sant-ana-407a3a143/
---
+++ script.js
@@ -0,0 +1,49 @@
+document.getElementById("concluido").style.visibility = "hidden";
+
+function testeCPF(strCPF) {
+  var soma;
+  var resto;
+  soma = 0;
+  
+  if (strCPF == "00000000000") return false;
+  
+  for (i=1; i<=9; i++) soma = soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+  resto = (soma * 10) % 11;
+  
+  if ((resto == 10) || (resto == 11))  resto = 0;
+  if (resto != parseInt(strCPF.substring(9, 10))) return false;
+  
+  soma = 0;
+  for (i = 1; i <= 10; i++) soma = soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+  resto = (soma * 10) % 11;
+  
+  if ((resto == 10) || (resto == 11))  resto = 0;
+  if (resto != parseInt(strCPF.substring(10, 11))) return false;
+  
+  return true;
+}
+  
+function validarCpf() {
+  var cpfValor = document.getElementById("cpf").value;
+  
+  if (testeCPF(cpfValor) == true) {
+    document.getElementById("Vcpf").innerHTML = "CPF VÁLIDO";
+    document.getElementById("Vcpf").style.fontWeight = "600";
+    document.getElementById("Vcpf").style.color = "green";
+} 
+    
+  if (testeCPF(cpfValor) == false) {
+    document.getElementById("Vcpf").innerHTML = "CPF INVÁLIDO";
+    document.getElementById("Vcpf").style.fontWeight = "600";
+    document.getElementById("Vcpf").style.color = "red";
+}
+  
+  if (cpfValor == "") {
+    document.getElementById("Vcpf").innerHTML = "CPF <span>*</span>";
+    document.getElementById("Vcpf").style.fontWeight = "400";
+    document.getElementById("Vcpf").style.color = "black";
+  }
+}
+
+
+
